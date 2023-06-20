5d9e948b67072ca79742c654fe73009e649e3c82
Krishna Annam <annamkishore@gmail.com>
2023-06-20 12:59:44 +0530

    meta folder for setup & config, etc

--- counter-web/build-deploy.sh
+++ counter-web/build-deploy.sh
@@ -1,7 +1,7 @@
 # remove existing deployment, service in kube
 
-docker build . -t localhost:32000/container-web-tag:registry
-docker push localhost:32000/container-web-tag:registry
+docker build . -t localhost:32000/container-web-tag-3:hello3
+docker push localhost:32000/container-web-tag-3:hello3
 
 kubectl apply -f ../kube-conf/d1-deployment-web.yaml
 kubectl expose deployment counter-web-deployment  --type="LoadBalancer"
